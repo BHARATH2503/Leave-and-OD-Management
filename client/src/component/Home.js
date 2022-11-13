3063e2c172f420ef78c2c5702ab4de1258f0ca34
Bharath <95229845+BHARATH2503@users.noreply.github.com>
2022-11-13 23:33:32 +0530

    marquee tag in home

--- client/src/component/Home.js
+++ client/src/component/Home.js
@@ -1,28 +1,34 @@
 import React from "react";
 import logo2 from "../images/logo-2.jpg";
 import "../css/Home.css";
+
+
 function Home() {
     return (
-        <div style={{marginLeft:"1%"}}>
+        <div style={{ marginLeft: "1%" }}>
             <br></br>
-            <img src={logo2} alt="photokec" width="55%" height="500em"/>
+            <img src={logo2} alt="photokec" width="55%" height="500em" />
             <div className="centered cont">
-            <pre>
-            <h2>
-                    LEAVE
-                    <br></br>AND
-                    <br></br>ON DUTY
-                    <br></br>MANAGEMENT 
-                    <br></br>SYSTEM
-                </h2>
-            </pre>
+                <pre>
+                    <h2>
+                        LEAVE
+                        <br></br>AND
+                        <br></br>ON DUTY
+                        <br></br>MANAGEMENT
+                        <br></br>SYSTEM
+                    </h2>
+                </pre>
             </div>
             <br></br>
             <br></br>
             <br></br>
-            <marquee><h3 style={{fontWeight:"600",fontSize:"130%"}}>Hello ! welcome to Leave and On Duty Management System . If you want to see Leave and OD Details click on View . To enter Leave and OD details click on Entry . It is only applicable for Faculty .</h3></marquee>
+
+            <div class="marquee">
+            <h3>Hello ! welcome to Leave and On Duty Management System . If you want to see Leave and OD Details click on View . To enter Leave and OD details click on Entry . It is only applicable for Faculty .</h3>
+            </div>
+
         </div>
-        
+
     )
 }
 export default Home;
